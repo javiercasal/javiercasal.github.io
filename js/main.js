6d70eda15aff4c75630afa36f9e9560dc79ca26f
dieteticaaxelyjavi <dieteticaaxelyjavi@gmail.com>
2025-06-27 14:12:33 -0300

    Fix referencia a github

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
  */
 async function cargarYMostrarProductos() {
     try {
-        const urlCSV = 'https://raw.githubusercontent.com/dieteticaaxelyjavi/productos/refs/heads/main/lista.csv';
+        const urlCSV = 'https://raw.githubusercontent.com/dieteticaaxelyjavi/productos/main/lista.csv';
         const respuesta = await fetch(urlCSV);
         const textoCSV = await respuesta.text();
 
