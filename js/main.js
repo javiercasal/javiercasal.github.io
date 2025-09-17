4bacb861afbf50cd723c46d7fc0a1c533a8e4aa9
dieteticaaxelyjavi <dieteticaaxelyjavi@gmail.com>
2025-09-17 11:33:59 -0300

    Fix url

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ function deselectAllTags() {
 // Carga el archivo CSV desde GitHub, lo parsea y muestra los productos
 async function cargarYMostrarProductos() {
     try {
-        const urlCSV = 'https://raw.githubusercontent.com/dietetica/productos/main/prueba.csv';
+        const urlCSV = 'https://raw.githubusercontent.com/dietetica/datos/main/productos.csv';
         const respuesta = await fetch(urlCSV);
         const textoCSV = await respuesta.text();
 
